e72f307aed55ba43e81fd3b057554fd050035930
Adit122022 <adity202212@gmail.com>
2025-01-13 12:15:19 +0530

    passing data to ejs  - version 002 version001 - where we passing just string and a single  object or single data version002- passing an array of objects and printing the data

--- Ankur Bhaiya/Backup/class1/app.js
+++ Ankur Bhaiya/Backup/class1/app.js
@@ -1,1 +1,36 @@
 const express = require('express')
+const app = express()
+
+app.set('view engine' , 'ejs');
+app.get('/', (req,res)=>{
+    // const data =new Date()
+    // res.render('index');  
+
+    // const formateDate = `${data.getDate() + '-' + data.getMonth() + '-' + data.getFullYear()}`;  // passing data to index.ejs
+    // console.log(formateDate)
+    // res.render('index', {date: formateDate})   // passing data to index.ejs via ejs template engine      ejs - <%= date %>
+
+    // pasing objects
+    const person = [{
+        name: "John Doe",
+        age: 25,
+        city: "New York",
+        image:"https://preview.redd.it/is-this-the-best-panel-in-naruto-v0-ezc3da6ucoce1.jpeg?width=1080&crop=smart&auto=webp&s=b0bfef872ee714ca65d17640f4a4e57194a1bc3a"
+    },
+    {
+        name: "JOKER Doe",
+        age: 25,
+        city: "New York",
+             image:"https://preview.redd.it/is-this-the-best-panel-in-naruto-v0-ezc3da6ucoce1.jpeg?width=1080&crop=smart&auto=webp&s=b0bfef872ee714ca65d17640f4a4e57194a1bc3a"
+    },
+]
+
+    res.render('index', {date :person})  // passing data to index.ejs via ejs template engine      ejs - <%= person.name %>  <%= person.age %>  <%= person.city %>
+
+})
+app.get('*', (req,res)=>{
+res.render('notFound')
+})
+app.listen(3000, ()=>{
+    console.log("SERVER is started on  port 3000")
+})
