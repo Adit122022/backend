23963ad555dd32d23fa53ecab9aa1ec1d53216e6
Adit122022 <adity202212@gmail.com>
2025-01-14 11:49:57 +0530

    version 002  -> layer three index.controller.js

--- Ankur Bhaiya/class2/src/app.js
+++ Ankur Bhaiya/class2/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express();
 const controller = require('./controllers/index.controller')
+
 app.get('/',controller.indexController); 
+app.get('/about',controller.aboutController); 
 module.exports = app;
--- Ankur Bhaiya/class2/src/controllers/index.controller.js
+++ Ankur Bhaiya/class2/src/controllers/index.controller.js
@@ -1,3 +1,6 @@
 module.exports.indexController =(req,res)=>{
     res.send('HElll ooooooooooooooooo')
 }
+module.exports.aboutController =(req,res)=>{
+    res.send('Hello from About page')
+}
