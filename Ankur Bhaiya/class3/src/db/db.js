9f062c613be6ddd9b55195b748695f6b3d20a68b
Adit122022 <adity202212@gmail.com>
2025-01-16 14:25:30 +0530

    bare miniumum for   creating server

--- Ankur Bhaiya/class3/src/db/db.js
+++ Ankur Bhaiya/class3/src/db/db.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/class3')          // 0.0.0.0  , for localhost   -  class3 is the name of the databse
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
+
+    const userSchema = new mongoose.Schema({
+        username: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true}
+    })
+    
+    module.exports = mongoose.model('User', userSchema);
