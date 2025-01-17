c3fef576d261d6dd04ad6a7f450405aeed7d61a8
Adit122022 <adity202212@gmail.com>
2025-01-17 11:46:27 +0530

    version 003 -->  separating the userSchema and  using it into index.controllers

--- Ankur Bhaiya/class5/src/controllers/index.controller.js
+++ Ankur Bhaiya/class5/src/controllers/index.controller.js
@@ -1,3 +1,8 @@
+const userModel = require('../models/user.model')
 module.exports.indexController =(req,res)=>{
     res.send("HELLO WORLD!");
+}
+module.exports.registerController =(req,res)=>{
+    console.log(req.query)
+    res.send("Register");
 }
--- Ankur Bhaiya/class5/src/routes/index.routes.js
+++ Ankur Bhaiya/class5/src/routes/index.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {indexController} = require('../controllers/index.controller')
+const {registerController} = require('../controllers/index.controller')
 
 // middleware to log requests
 router.get('/',indexController )
-module.exports = router
+router.get('/register' , registerController)
+module.exports = router;
