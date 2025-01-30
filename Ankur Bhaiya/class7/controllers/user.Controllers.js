6830cdb7b7437848501820dc709f1562229b5db5
Adit122022 <adity202212@gmail.com>
2025-01-30 14:58:42 +0530

    jsut for some authorization

--- Ankur Bhaiya/class7/controllers/user.Controllers.js
+++ Ankur Bhaiya/class7/controllers/user.Controllers.js
@@ -32,10 +32,10 @@ module.exports.loginUser = async(req, res) => {
 }
 
 module.exports.profile = async(req, res) => {
-try{  const decode = jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_SECRET);
+try{  const decode = jwt.verify(req.headers.authorization?.split(' ')[1], process.env.JWT_SECRET);
     if(!decode) return res.status(401).json({message:'Token not provided'});
     const user = await UserModel.findById({_id :decode._id});
-    if(!user) return res.status(404).json({message:'User not found'});
+    if(!user) return res.status(401).json({message:'User not found'});
     res.status(200).json({user});
 }catch(err){
     res.status(400).json({message:err.message});
