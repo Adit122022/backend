3919f3c04d96e1d0ca7ac7e7e3883ff2f58f3b9f
Adit122022 <adity202212@gmail.com>
2025-01-30 14:01:22 +0530

    token created in class7

--- Ankur Bhaiya/class7/controllers/user.Controllers.js
+++ Ankur Bhaiya/class7/controllers/user.Controllers.js
@@ -1,5 +1,6 @@
 const { UserModel } = require("../models/user.models")
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports.registerUser = async(req, res) => {
 try{
@@ -18,10 +19,26 @@ module.exports.loginUser = async(req, res) => {
         const {email , password} = req.body;
         const user = await UserModel.findOne({email});
         if(!user) return res.status(400).json({message:'email or password not found!'});
+
         const isMatch = await bcrypt.compare(password, user.password); // this return a promise  and after the promis resolved it return a boolean value
         if(!isMatch) return res.status(400).json({message:'email or password not found!'});
-        res.status(200).json({message:'Login successful ✅✅✅'});
+        
+    const token =  jwt.sign({_id:user._id ,}, process.env.JWT_SECRET);
+        res.status(200).json({message:'Login successful ✅✅✅' , token});
 
+    }catch(err){
+        res.status(400).json({message:err.message});
+    }
+}
+
+module.exports.profile = async(req, res) => {
+
+    try{
+
+        const user = await UserModel.findById(req.user._id);
+
+
+        res.status(200).json({user});
     }catch(err){
         res.status(400).json({message:err.message});
     }
--- Ankur Bhaiya/class7/routes/user.routes.js
+++ Ankur Bhaiya/class7/routes/user.routes.js
@@ -1,7 +1,8 @@
 const {Router}  = require('express');
-const { registerUser, loginUser } = require('../controllers/user.Controllers');
+const { registerUser, loginUser, profile } = require('../controllers/user.Controllers');
 const router = Router();
 
 router.post('/register', registerUser);
-router.post('/login', loginUser)
+router.post('/login', loginUser);
+router.get('/profile', profile)
 module.exports = router;
