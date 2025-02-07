100cc4992db392ba543ccca611e63b678a7bd7c7
Adit122022 <adity202212@gmail.com>
2025-02-07 14:42:38 +0530

    version 001  ---> 001 userschema is created

--- Ankur Bhaiya/PROJECTS/INSTAGRAM/BACKEND/src/models/userModels.js
+++ Ankur Bhaiya/PROJECTS/INSTAGRAM/BACKEND/src/models/userModels.js
@@ -5,5 +5,9 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, trim:true, unique: true, minlength: [3,'email must be at least 3 characters'], maxlength: [30 , 'email must be at most 30 characters']},
     password: {  type: String,    },
     profilePicture: {  type: String ,default : "https://i.pinimg.com/736x/d4/22/47/d4224772b36eb181979c343e8f087082.jpg"},
-    
+    posts:[{   type: mongoose.Schema.Types.ObjectId,  ref: 'Post' }] ,
+    followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    following: [{type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
 })
+
+module.exports = mongoose.model('user', UserSchema);
